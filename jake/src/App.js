5675c9c119480fd33f7a14a0c7437f93155d65a2
Jake Duff <42357602+jaked3@users.noreply.github.com>
2018-08-24 16:34:25 -0400

    adding preliminary files

---
+++ jake/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './Components/Navigation';
+
+class App extends Component {
+  render() {
+    return (
+      //< Navbar />
+
+      <div>
+        <Navigation />
+        <div className="container">
+         <h1> Jaaake </h1>
+
+        </div>
+      </div>
+
+
+        // <div className="App">
+         //   <header className="App-header">
+           
+            
+          //   </header>
+                  // </div>
+    );
+  }
+}
+
+export default App;
---
+++ jake/src/Components/Navigation.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+
+const NavItem = props => {
+  const pageURI = window.location.pathname+window.location.search
+  const liClassName = (props.path === pageURI) ? "nav-item active" : "nav-item";
+  const aClassName = props.disabled ? "nav-link disabled" : "nav-link"
+  return (
+    <li className={liClassName}>
+      <a href={props.path} className={aClassName}>
+        {props.name}
+        {(props.path === pageURI) ? (<span className="sr-only">(current)</span>) : ''}
+      </a>
+    </li>
+  );
+}
+
+
+class Navigation extends React.Component {
+render(){
+
+	return(
+		<div>
+		 <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  			<a className="navbar-brand" href="/">Jake</a>
+ 			 <button className="navbar-toggler" type="button" 
+ 			 data-toggle="collapse" data-target="#navbarNavDropdown"
+ 			  aria-controls="navbarNavDropdown" aria-expanded="false" 
+ 			  aria-label="Toggle navigation">
+   				 <span className="navbar-toggler-icon"></span>
+ 			 </button>
+
+			  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+			    <ul className="navbar-nav">
+
+			      <NavItem path="/" name="Home" />
+			      <NavItem path="/" name="About" />
+			      <NavItem path="/" name="Contact" />
+
+
+
+			      <li className="nav-item dropdown">
+			        <a className="nav-link dropdown-toggle"
+			         href="/" id="navbarDropdownMenuLink" role="button" 
+			         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			          Dropdown link
+			        </a>
+			        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+			          <a className="dropdown-item" href="/">Action</a>
+			          <a className="dropdown-item" href="/">Another action</a>
+			          <a className="dropdown-item" href="/">Something else here</a>
+			        </div>
+			      </li>
+
+			    </ul>
+			  </div>
+			</nav>
+ 		</div>
+
+	);
+
+}
+
+
+
+
+}
+
+export default Navigation;
+
+//https://m.pardel.net/react-and-bootstrap-4-part-1-setup-navigation-d4767e2ed9f0
